8f0f7f0276825431ec0706681ec98fd18aa5c5c5
RusuI <ioanarusu725@gmail.com>
2022-04-13 19:29:49 +0300

    commit 3

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 import KanbanAPI from "./api/api.js";
+import Kanban from "./view/Kanban.js";
 
-// KanbanAPI.insertItem(1, "Ioanaa");
-
-KanbanAPI.updateItem(79708, {
-    columnId: 2,
-    position: 0,
-    content: "buzz"
-})
+new Kanban(document.querySelector(".kanban"))
 
